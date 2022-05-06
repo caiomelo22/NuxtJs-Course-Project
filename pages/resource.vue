c06632bdee5d12d0ba10a66b5662c4c43417c575
Caio Graça Melo <49076270+caiomelo22@users.noreply.github.com>
2022-05-06 09:23:56 -0300

    Assets/static diff

---
+++ pages/resource.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="resource">
+    <img src="~/assets/joao frango.jpg">
+    <hr />
+    <img src="/r10.jpg">
+    <hr/>
+    <!-- <div id="frame"></div> -->
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+#resource {
+  text-align: center;
+}
+#frame {
+  margin: 0; width: 300px; height: 225px; background-image: url('/r10.jpg');
+}
+</style>
